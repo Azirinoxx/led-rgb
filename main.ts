7d5f29da9eb7b63bcb8a8e642d013e75155ccc59
Azirinoxx <93145316+Azirinoxx@users.noreply.github.com>
2021-11-16 10:14:05 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+let intensite = 0
+function bleu () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function blanc () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function mauve () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+}
+function rouge () {
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+}
+function vert () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    pins.digitalWritePin(DigitalPin.P8, 0)
+}
+basic.forever(function () {
+    intensite = pins.analogReadPin(AnalogPin.P0)
+    if (intensite >= 700) {
+        vert()
+    } else if (intensite < 699 && intensite >= 501) {
+        bleu()
+    } else if (intensite < 500 && intensite >= 301) {
+        mauve()
+    } else if (intensite < 300 && intensite >= 151) {
+        blanc()
+    } else if (intensite < 150 && intensite >= 0) {
+        rouge()
+    }
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
+})
